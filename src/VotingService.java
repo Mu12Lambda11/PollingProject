19fb62b52b3999a437c31393d5a56f0673e37065
Isaiah Jordan <idjordan@cpp.edu>
2023-10-03 17:19:37 -0700

    Finished VotingService

--- src/VotingService.java
+++ src/VotingService.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class VotingService {
     
     private int aCount=0;
@@ -6,6 +8,8 @@ public class VotingService {
     private int dCount=0;
     private int trueCount=0;
     private int falseCount=0;
+    private ArrayList<String> studentSubmissions= new ArrayList<String>();
+    private ArrayList<String> studentSubmissionIDs= new ArrayList<String>();
     private Question configuredQuestion;
     private String questionText=configuredQuestion.getQuestion();
     private String questionAnswer=configuredQuestion.getAnswer();
@@ -17,10 +21,27 @@ public class VotingService {
     }
 
 
+    //Accept student submissions after verifying their ID
+    void acceptSubmission(Student student){
+    //if ID array does not contain student's ID, ...
+    if(!(studentSubmissionIDs.contains(student.getID()))){
+        studentSubmissionIDs.add(student.getID());
+        studentSubmissions.add(student.getStudentAnswer());
+    }else{
+        //if ID array does contain student's ID, find and replace their answer
+        for(int i =0; i<studentSubmissionIDs.size();i++){
+            if(studentSubmissionIDs.get(i)==student.getID()){
+                studentSubmissionIDs.set(i, student.getID());
+                studentSubmissions.set(i, student.getStudentAnswer());
+            }
+        }
+    }
+    }
 
     //Processes all votes, and increments the respective vote counter
-    void processVotes(String answer){
-        switch(answer.toLowerCase()){
+    void processVotes(ArrayList answers){
+        for(int i=0; i<answers.size();i++){
+            switch(((String)answers.get(i)).toLowerCase()){
             case "a":
             aCount++;
             break;
@@ -44,8 +65,20 @@ public class VotingService {
             case "false":
             falseCount++;
             break;
+             }
         }
+    }
 
+    //Resets the studentSubmission and count variables to prepare for the next question
+    void resetVariables(){
+    aCount=0;
+    bCount=0;
+    cCount=0;
+    dCount=0;
+    trueCount=0;
+    falseCount=0;
+    studentSubmissions= new ArrayList<String>();
+    studentSubmissionIDs= new ArrayList<String>();
     }
 
     //Print out the configured question
